c5b56033d1d80747437cce9e730da9f5f25fc7b3
TuNombre <johantirado24@gmail.com>
2025-05-31 23:30:56 -0500

    If and Switch

---
+++ Variables/SentenciaIfElseIf/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenciaIfElseIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool areYouHungry=true;
+            bool youHaveMoney = false;
+
+            if (areYouHungry&&youHaveMoney)
+            {
+                Console.WriteLine("Come");
+            }
+        }
+    }
+}
---
+++ Variables/Switch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 1;
+            switch (op)
+            {
+                case 1:
+                    Console.WriteLine("Seleccionaste 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Seleccionaste el 2");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("Selecdionaste la opción 3 o 4");
+                    break;
+                default:
+                    Console.WriteLine("Invalido");
+                    break;
+            }
+
+        }
+    }
+}
