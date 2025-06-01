3438fcce85735795732fc09fe9db832ff2c8a0d8
TuNombre <johantirado24@gmail.com>
2025-05-31 22:59:53 -0500

    Tipo de Datos

---
+++ Variables/TipodeDato/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipodeDato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 123;
+            double doubleNumber = 12.123;
+            char character= 'A';
+            string name = "Johan Aristizabal Tirado";
+            bool isDead = false;
+
+            Console.WriteLine(isDead);
+        }
+    }
+}
