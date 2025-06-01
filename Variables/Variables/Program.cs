74009b6f62295ec4ff6130b4cffd9157b69e7936
johanfeb24 <johantirado24@gmail.com>
2025-05-31 22:22:13 -0500

    Introduccion a C#

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 1;
+            int number2 = 2;
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+        }
+    }
+}
